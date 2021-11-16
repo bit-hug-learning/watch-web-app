6a063ba3da4514e7fa0df3915c9ef1a257bc6186
Gonzo <gvidal.industrial@gmail.com>
2021-11-16 12:43:50 -0500

    Fix countdown timer bug and add styles to success and warning messages

--- src/scripts/countdown.js
+++ src/scripts/countdown.js
@@ -8,49 +8,52 @@ let cont = 0;
 let isPaused = false;
 let isReset = false;
 let isOn = false;
-let timeUpCounter = 0;
+let timeUpCounter = 0; 
+let p = document.createElement("p");
+p.classList.add("warning-msg");
 
 buttonStart.addEventListener("click", go);
 
 function go() {
-  let p = document.createElement("p");
   let display = text;
   let hours, minutes, seconds;
   let duration = input.value;
   let tick;
   timeUpCounter > 0 && text.nextSibling.remove(); 
-  p.style.fontSize = "2rem";
+  
   buttonStart.setAttribute("disabled", true);
+  display.insertAdjacentElement("afterend", p);
+  
+  if (input.value != "" && typeof(parseInt(input.value,10)) != "number" || input.value == parseInt(input.value,10)) {
+    let timer = duration;
+    p.textContent = "";
 
-    display.insertAdjacentElement("afterend", p);
-    if (input.value != "" && !isNaN(parseInt(input.value))) {
-      let timer = duration;
-      p.textContent = "";
-
-      tick = setInterval(() => {
-          if(!isPaused) {
-            hours = parseInt(timer / 3600, 10);
-            minutes = parseInt(timer % 3600 / 60 , 10);
-            seconds = parseInt(timer % 3600 - 60*parseInt(timer % 3600 / 60,10), 10);
-            
-            hours = hours < 10 ? "0" + hours : hours;
-            minutes = minutes < 10 ? "0" + minutes : minutes;
-            seconds = seconds < 10 ? "0" + seconds : seconds;
-            console.log(hours, minutes, seconds)
-            display.textContent = `${hours}:${minutes}:${seconds}`;
-            
-            if (--timer < 0) {
-              buttonStart.removeAttribute("disabled")
-              p.textContent = "Time's up!!"
-              clearInterval(tick);
-              timeUpCounter++;
-            }
-          }  
-             }, 1000)
+    tick = setInterval(() => {
+      if(!isPaused) {
+        hours = parseInt(timer / 3600, 10);
+        minutes = parseInt(timer % 3600 / 60 , 10);
+        seconds = parseInt(timer % 3600 - 60*parseInt(timer % 3600 / 60,10), 10);
+        
+        hours = hours < 10 ? "0" + hours : hours;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        console.log(hours, minutes, seconds)
+        display.textContent = `${hours}:${minutes}:${seconds}`;
+        
+        if (--timer < 0) {
+          buttonStart.removeAttribute("disabled");
+          p.textContent = "Time's up!!";
+          p.style.backgroundColor = "#b3f3b1";
+          clearInterval(tick);
+          timeUpCounter++;
+        }
+      }  
+  }, 1000)
     } else {
-      cont == 0 ?
-      p.textContent = "Insert a number" : "";
+      p.textContent = "Insert a valid number"; 
+      p.style.backgroundColor = "#f3b1b1"   
       cont++;
+      buttonStart.removeAttribute("disabled");
       }
 
   buttonPause.addEventListener("mouseup", pause);
